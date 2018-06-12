cf003e2e9ff8e7b1feb937a088d039f325aa455d
Hugo Castelani <HugoCastelaniBP@gmail.com>
2018-06-12 16:46:45 -0300

    Additional commit Change Google Sans to Google Sans bold; Change Calligraphy to Font Utils; Add missing translation; Update readme.

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ This is a tiny android app that deletes WhatsApp sent videos. I actually made it
 
 Libraries used (thanks to the creators):<br>
 Material Colors: https://github.com/theapache64/material_colors<br>
-Calligraphy: https://github.com/InflationX/Calligraphy<br>
-View Pump: https://github.com/InflationX/ViewPump<br>
+Font Utils: https://github.com/androidmads/FontUtils<br>
 Dexter: https://github.com/Karumi/Dexter<br>
 Android Storage: https://github.com/sromku/android-storage<br>
--- app/src/main/java/com/hugocastelani/maid/MainActivity.kt
+++ app/src/main/java/com/hugocastelani/maid/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.hugocastelani.maid
 
 import android.Manifest
-import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -11,12 +10,13 @@ import com.karumi.dexter.Dexter
 import com.karumi.dexter.listener.single.PermissionListener
 import com.karumi.dexter.listener.single.SnackbarOnDeniedPermissionListener
 import com.snatik.storage.Storage
-import io.github.inflationx.viewpump.ViewPumpContextWrapper
 import android.content.pm.PackageManager
+import android.graphics.Typeface
 import android.net.Uri
 import android.support.v4.app.ActivityCompat
-import android.os.Build
 import android.provider.Settings
+import com.ajts.androidmads.fontutils.FontUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +29,10 @@ class MainActivity : AppCompatActivity() {
 
         storage = Storage(applicationContext)
         directory = "${storage.externalStorageDirectory}/WhatsApp/Media/WhatsApp Video/Sent"
+
+        // Applying Custom Font
+        val typeface = Typeface.createFromAsset(assets, "fonts/Google-Sans-Bold.ttf")
+        FontUtils().applyFontToView(toolbar_title, typeface)
 
         Dexter.withActivity(this)
             .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -51,7 +55,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(
                 findViewById(android.R.id.content),
                 resources.getString(R.string.all_clean),
-                5000
+                3000
             ).show()
 
         } else {
@@ -62,7 +66,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(
                 findViewById(android.R.id.content),
                 resources.getString(R.string.without_permission),
-                8000
+                5000
             ).setAction(
                 resources.getString(R.string.settings),
                 {
@@ -80,10 +84,5 @@ class MainActivity : AppCompatActivity() {
     private fun hasPermission(): Boolean {
         val permission = Manifest.permission.WRITE_EXTERNAL_STORAGE
         return ActivityCompat.checkSelfPermission(baseContext, permission) == PackageManager.PERMISSION_GRANTED
-    }
-
-    // calligraphy library need
-    override fun attachBaseContext(newBase: Context) {
-        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase))
     }
 }
